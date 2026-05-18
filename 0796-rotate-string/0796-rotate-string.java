5a00b1b39c47eeda8aef0df051341dbd18228c01
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-05-18 07:59:17 +0700

    Delete 0796-rotate-string directory

--- 0796-rotate-string/0796-rotate-string.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public boolean rotateString(String s, String goal) {
-        String combined = s + s;
-        if (s.length() != goal.length()) return false;
-        if (combined.contains(goal)) return true;
-        else return false;
-    }
-}
