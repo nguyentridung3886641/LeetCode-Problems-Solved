cedc4a0e96431b32c87933e4b20a7f7375db1663
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-05-21 05:39:24 +0700

    Time: 0 ms (100%), Space: 45.5 MB (58.28%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return maxMinDepth(root) >= 0;
+    }
+    public int maxMinDepth(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = maxMinDepth(node.left);
+        int right = maxMinDepth(node.right);
+
+        if (left == - 1 || right == - 1) return -1;
+
+        return (Math.abs(left - right) <= 1) ? 1 + Math.max(left, right) : -1;
+    }
+}
