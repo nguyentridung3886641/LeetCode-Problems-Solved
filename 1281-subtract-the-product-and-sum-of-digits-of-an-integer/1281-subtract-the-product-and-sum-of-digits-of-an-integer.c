a4dd98e11ebe303859de0e1a11864fa3e4e6e808
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-08-10 16:40:51 +0700

    Time: 0 ms (100%), Space: 8.5 MB (51.8%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.c
@@ -0,0 +1,12 @@
+int subtractProductAndSum(int n) {
+    int sum = 0, product = 1;
+
+    while (n != 0) {
+        int temp = n % 10;
+        sum += temp;
+        product *= temp;
+        n /= 10;
+    }
+
+    return product - sum;
+}
