0250f1988deb9c041886a9298678152cc974d950
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-08-31 11:54:35 +0700

    Time: 2 ms (99.85%), Space: 53.4 MB (83.13%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] count = new int[n + 1];
+        for (int[] i : trust) {
+            --count[i[0]];
+            ++count[i[1]];
+        }
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == n - 1)
+                return i;
+        }
+        return -1;
+    }
+}
