95c475fb7146ad9b53212000378b10ceb20d8790
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-09-02 21:18:55 +0700

    Time: 0 ms (100%), Space: 47.1 MB (10.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int original = image[sr][sc];
+        if (original == color) return image;
+
         int rowLimit = image.length - 1, colLimit = image[0].length - 1;
-        int oldColor = image[sr][sc];
-        if (oldColor == color) return image;
-        dfs(sr, sc, colLimit, rowLimit, oldColor, color, image);
+        dfs(sr, sc, original, color, colLimit, rowLimit, image);
+        
         return image;
     }
-    public void dfs(int row, int col, int colLimit, int rowLimit, int oldColor, int color, int[][] image) {
-        if (row > rowLimit || col > colLimit || col < 0 || row < 0) return;
-        if (image[row][col] != oldColor) return;
-        else {
-            image[row][col] = color;
+    public static void dfs(int row, int column, int original, int color, int colLimit, int rowLimit, int image[][]) {
+        if (row > rowLimit || column > colLimit || row < 0 || column < 0) return;
+        if (image[row][column] != original) {
+            return;
+        } else {
+            image[row][column] = color;
         }
-        dfs(row + 1, col, colLimit, rowLimit, oldColor, color, image);
-        dfs(row - 1, col, colLimit, rowLimit, oldColor, color, image);
-        dfs(row, col + 1, colLimit, rowLimit, oldColor, color, image);
-        dfs(row, col - 1, colLimit, rowLimit, oldColor, color, image);
+        dfs(row - 1, column, original, color, colLimit, rowLimit, image);
+        dfs(row + 1, column, original, color, colLimit, rowLimit, image);
+        dfs(row, column - 1, original, color, colLimit, rowLimit, image);
+        dfs(row, column + 1, original, color, colLimit, rowLimit, image);
     }
 }
