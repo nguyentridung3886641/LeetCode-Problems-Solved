ac7ae19d1cd9db3c71dc42118432485b361338b0
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-09-03 13:19:26 +0700

    Time: 5 ms (83.33%), Space: 121.6 MB (62.32%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
         if (nums1.length == 1) return true;
+
         int minimum = Integer.MAX_VALUE;
+        boolean hasOdd = false;
+
         for (int i = 0; i < nums1.length; i++) {
             minimum = Math.min(minimum, nums1[i]);
+            if (nums1[i] % 2 != 0) {
+                hasOdd = true;
+            }
         }
 
-        if (minimum % 2 == 1)
+        if (minimum % 2 != 0) {
             return true;
-
-        for (int num : nums1) {
-            if (num % 2 == 1)
-                return false;
+        } else if (hasOdd == true) {
+            return false;
         }
 
         return true;
