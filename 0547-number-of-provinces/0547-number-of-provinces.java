177d5f537058f4b617cffdf70b5a588f20d2a31b
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-09-06 22:23:25 +0700

    Time: 2 ms (41.72%), Space: 47 MB (88%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,52 @@
+class Solution {
+    static int provinces;
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        provinces = n;
+        int[] parents = new int[n];
+        int[] size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+            size[i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && j != i) {
+                    union(i, j, parents, size);
+                }
+            }
+        }
+
+        return provinces;
+    }
+
+    public static int find(int i, int[] parents) {
+        if (parents[i] == i) {
+            return i;
+        }
+        return parents[i] = find(parents[i], parents);
+    }
+
+    public static boolean union(int x, int y, int[] parents, int[] size) {
+        int rootX = find(x, parents);
+        int rootY = find(y, parents);
+
+        if (rootX == rootY) {
+            return false;
+        }
+
+        if (size[rootX] < size[rootY]) {
+            int temp = rootX;
+            rootX = rootY;
+            rootY = temp;
+        }
+
+        parents[rootY] = rootX;
+        size[rootX] += size[rootY];
+        --provinces;
+
+        return true;
+    }
+}
